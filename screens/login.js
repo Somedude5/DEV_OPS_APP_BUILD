41edf2d011b55f5e5dd83a99f673e3ee43d89580
Somedude5 <parkerhatch2098@gmail.com>
2022-11-17 11:19:18 -0700

    changes

---
+++ screens/login.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch';
+const login = async ()=>{
+    const tokenResponse = await fetch('https://dev.stedi.cayson.cit262.com/login%27,%7B
+        method: 'POST',
+        body:JSON.stringify({
+          userName: 'notrealemail@gmail.com',
+          password:'P@ssw0rd'
+        })
+    })
+  return tokenResponse;
+};
+export default login;
---
+++ tests/hello.js
@@ -0,0 +1,7 @@
+const hello = ()=>{
+
+
+    console.log("hello!");
+}
+
+export {hello};
